98fc879b89b6914c4795ed0e975e19abdc2b34eb
chernykh654 <chernykh.progect@gmail.com>
2017-08-25 16:44:32 +0500

    Морской бой 1.0

---
+++ SeaBattleGame/src/SeaBattleGame/DotCom.java
@@ -0,0 +1,22 @@
+package SeaBattleGame;
+
+/**
+ * Created by Сергей on 09.08.2017.
+ */
+import java.util.*;
+
+public class DotCom {
+    private ArrayList<String> locationCells;
+    private String name;
+
+    public  void  setLocationCells(ArrayList<String> loc) {
+        locationCells = loc;
+    }
+
+    public void setName(String n) {
+        name = n;
+    }
+
+    public String checkY
+
+}
